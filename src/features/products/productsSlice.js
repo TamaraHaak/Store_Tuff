63b0174f98018f5549ffb31b9c0dcc064f4cd9a3
Maryam <tamarahaak@web.de>
2024-05-14 12:38:35 +0200

    axios

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+import axios from 'axios';
 
 import { BASE_URL } from "../../utils/constants";
 import { shuffle } from "../../utils/common";
